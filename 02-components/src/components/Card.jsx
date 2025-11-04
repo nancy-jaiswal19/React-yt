ff352d07b848789f78cb1ad498b82051ce31f9f5
nancy-jaiswal19 <nancyjais1907@gmail.com>
2025-11-04 16:36:02 +0530

    02-components done

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className = "card">
+        <h1>Nancy Jaiswal</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, itaque!</p>
+    </div>
+    </div>
+  )
+}
+
+export default Card
