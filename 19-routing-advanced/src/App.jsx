743e7f636e0752ca63346eff9c291896724d60f0
nancy-jaiswal19 <nancyjais1907@gmail.com>
2025-11-19 23:59:04 +0530

    Updated

---
+++ 19-routing-advanced/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navber from './components/Navbar'
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
